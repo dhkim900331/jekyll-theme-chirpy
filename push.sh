8ba9b4bde55441e8f4e166d7af4c276d32afd134
dhkim900331 <ks900331@naver.com>
2021-12-04 17:02:09 +0900

    [211204_1702] a

--- push.sh
+++ push.sh
@@ -2,9 +2,17 @@
 cd "${HOME}/${GIT_HOME}"
 echo "$GIT_HOME"
 
+LOG_TIME=`date +%y%m%d_%H%M`
+TITLE=$1
+
+if [ "e${TITLE}" == "e" ]; then
+     echo "push 'commit title'"
+     exit;
+fi
+
 git config --global user.email "ks900331@naver.com"
 git config --global user.name "dhkim900331"
 
 git add .
-git commit -m "a"
+git commit -m "[${LOG_TIME}] ${TITLE}"
 git push origin master
